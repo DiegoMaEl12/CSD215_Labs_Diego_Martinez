98edf93a2c15dbe3436a1f90da50f7606adba340
DiegoMaEl <24040549@saultcollege.ca>
2025-11-05 10:20:50 -0500

    Added some tests

--- src/test/java/Lab1/TaskTest.java
+++ src/test/java/Lab1/TaskTest.java
@@ -1,22 +1,59 @@
 package Lab1;
 
+import org.junit.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
-class TaskTest {
+public class TaskTest {
+    private TaskList tasks;
+    private Task task1;
+    private Task task2;
 
-    @org.junit.jupiter.api.Test
-    void getDescription() {
+    @Test
+    public void testAddTask() {
+        tasks = new TaskList();
+        task1 = new Task("Task 1");
+        task2 = new Task("Task 2");
+        tasks.addTask(task1);
+        assertEquals(1, tasks.getTasks().size());
+        assertEquals("Task 1", tasks.getTasks().getFirst().getDescription());
     }
 
-    @org.junit.jupiter.api.Test
-    void getStatus() {
+    @Test
+    public void testCompleteTask() {
+        tasks = new TaskList();
+        task1 = new Task("Task 1");
+        tasks.addTask(task1);
+        assertFalse(tasks.getTasks().getFirst().getStatus());
+        task1.setStatus(true);
+        assertTrue(tasks.getTasks().getFirst().getStatus());
     }
 
-    @org.junit.jupiter.api.Test
-    void setStatus() {
+    @Test
+    public void testRemoveTask(){
+        tasks = new TaskList();
+        task1 = new Task("Task 1");
+        task2 = new Task("Task 2");
+        tasks.addTask(task1);
+        tasks.addTask(task2);
+        assertEquals(2, tasks.getTasks().size());
+        tasks.removeTask(task1);
+        assertEquals(1, tasks.getTasks().size());
+        assertEquals("Task 2", tasks.getTasks().getFirst().getDescription());
     }
 
-    @org.junit.jupiter.api.Test
-    void testToString() {
+    @Test
+    public void testDeleteCompletedTasks(){
+        tasks = new TaskList();
+        task1 = new Task("Task 1");
+        task2 = new Task("Task 2");
+        tasks.addTask(task1);
+        tasks.addTask(task2);
+        task1.setStatus(true);
+        tasks = tasks.deleteCompletedTasks(tasks);
+        assertEquals(1, tasks.getTasks().size());
+        assertEquals("Task 2", tasks.getTasks().getFirst().getDescription());
     }
+
+
 }
